16f003381000ee5fe7abb04bf03443700dead2d5
Jon Redeker <jon.redeker@gmail.com>
2018-01-24 13:31:18 -0500

    format app and add templates

--- app/app.py
+++ app/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+# IMPORTS
+
+
+from flask import Flask, render_template
 import psycopg2
 import yaml
+
+
+# APP CONFIG
+
 
 app = Flask(__name__)
 app.config.from_pyfile('configure.py')
@@ -12,6 +19,8 @@ db = config['database_cfg']
 connection = psycopg2.connect(**db)
 cursor = connection.cursor()
 
+# CONTROLLERS
+
 
 @app.route("/")
 def hello():
@@ -20,6 +29,11 @@ def hello():
         return "Database connected"
     except:
         return "Database not connected!"
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
 
 
 if __name__ == "__main__":
