3fbd61791b178f5fe13e447198b6366be8a4e834
Ben Marti <William.Marti@gfs.com>
2018-01-24 10:50:45 -0800

    rename the db connection route to 'dbtest' and match the function name.  Tear up and tear down the connection within the method itself. PEP8 formatting says to have module imports at the top of the py file

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
 import psycopg2
 import yaml
+
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_pyfile('configure.py')
@@ -9,18 +10,26 @@ config_file = open("config.yml", "r")
 config = yaml.load(config_file)
 db = config['database_cfg']
 
-connection = psycopg2.connect(**db)
-cursor = connection.cursor()
 
+@app.route("/dbtest")
+def dbtest():
 
-@app.route("/")
-def hello():
     try:
+
+        connection = psycopg2.connect(**db)
+        cursor = connection.cursor()
+
         cursor.execute("""SELECT * FROM {}""".format(config['tables']['main']))
+
+        connection.close()
+
         return "Database connected"
+
     except:
+
         return "Database not connected!"
 
 
 if __name__ == "__main__":
-    app.run()
+
+    app.run(host='0.0.0.0')
