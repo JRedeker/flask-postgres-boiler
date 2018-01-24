f0f74b6e5c5affc20d9bde1eadf283a73ae2a631
Jon Redeker <jon.redeker@gmail.com>
2018-01-24 13:42:21 -0500

    reformat readme and app.py

--- app/app.py
+++ app/app.py
@@ -30,10 +30,17 @@ def hello():
     except:
         return "Database not connected!"
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/500.html'), 500
+
 
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
+
+
+# 🚀 LAUNCH  🚀
 
 
 if __name__ == "__main__":
