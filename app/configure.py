e035cfa62bfef3079f415f6f27a2017832b1af81
Jon Redeker <jon.redeker@gmail.com>
2018-01-25 14:41:43 -0500

    indifferent variables for config

--- app/configure.py
+++ app/configure.py
@@ -6,7 +6,7 @@ threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
 
-DATABASE_URI = 'postgresql://localhost/quality_metrics_reg'
+DATABASE_URI = 'postgresql://localhost/name'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
